efd39fb24d2f719af771208d3d3a956cf0387d3d
Helleson Sillva <102758545+Helleson@users.noreply.github.com>
2022-05-16 14:58:15 -0300

    2

---
+++ java script/Aula 11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 78
+console.log(`A leocidade do seu carro é ${vel}Km/h`)
+if (vel>60){//condição simples.
+    console.log('Você ultrapassou a vlocidade permitida. MULTADO')
+}
+console.log('Dirija sempre usando cinto de segunrança')
---
+++ java script/Aula 11/ex009.js
@@ -0,0 +1,7 @@
+var país = 'Brasil'
+console.log(`vivendo em ${país}`)
+if(país == 'Brasil'){
+    console.log('Você Brasileiro')
+}else{
+    console.log('Você é Estrangeiro')
+}
