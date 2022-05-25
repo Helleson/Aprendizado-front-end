606d3b786a81c76f4a0fc0ef67e7fa787538cdc0
Helleson Sillva <souza.hellison@gmail.com>
2022-05-24 21:46:02 -0300

    21

--- java script/aula 12 ex/ex015/script.js
+++ java script/aula 12 ex/ex015/script.js
@@ -1,11 +1,51 @@
 function verificar(){
-    var data = new Date
-    var ano = data.getFullYear
+    var data = new Date()
+    var ano = data.getFullYear()
     var fano = document.getElementById('txano')
-    var res = document.getElementById('res')
-    if(fano.value.length == 0 || fano.value > ano){
+    var res = document.querySelector('div#res')
+
+    if(fano.value.length == 0 || Number(fano.value) > ano){
         alert('[ERRO] verifique os dados e tente novamente')
     }else{
-        alert('tudo ok')
+
+       var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+       var genero = ''
+       var img = document.createElement('img')
+       img.setAttribute('id', 'foto')
+       if(fsex[0].checked){
+           genero = 'Homen'
+           if(idade >=0 && idade < 10){
+               //criança
+               img.setAttribute('src', 'menino1.jpg')
+           }else if (idade < 21){
+               //jovem
+               img.setAttribute('src', 'menino2.jpg')
+           }else if(idade<50){
+               //adulto
+               img.setAttribute('src', 'menino3.jpg')
+           }else{
+               //idoso
+               img.setAttribute('src', 'menino4.jpg' )
+           }
+       }else if (fsex[1].checked){
+           genero = 'Mulher'
+           if(idade >=0 && idade < 10){
+            //criança
+            img.setAttribute('src', 'menina1.jpg')
+        }else if (idade < 21){
+            //jovem
+            img.setAttribute('src', 'menina2.jpg')
+        }else if(idade<50){
+            //adulto
+            img.setAttribute('src', 'menina3.jpg')
+        }else{
+            //idoso
+            img.setAttribute('src', 'menina4.jpg')
+        }
+       }
+    
+       res.innerHTML = `Detectamos ${genero} com ${idade} anos`
+       res.appendChild(img)
     }
 }
