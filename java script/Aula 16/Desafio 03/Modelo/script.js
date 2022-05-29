1f1a16b772adbbb2e001092c42efa612639cae33
Helleson Sillva <souza.hellison@gmail.com>
2022-05-29 19:32:38 -0300

    ASD

--- java script/Aula 16/Desafio 03/Modelo/script.js
+++ java script/Aula 16/Desafio 03/Modelo/script.js
@@ -1,21 +1,29 @@
+  let lis = document.getElementById('lista')
 function analise(){
     let num = document.getElementById('num')
-    let lis = document.getElementById('lista')
+  
     let n = Number(num.value)
-    if(n<1 || n>100){
-        alert('Não pode')
+    
+    if(n<1 || n>100 ){
+        alert('Valor invalido ou já encontrado na lista!')
     }else{
-        
         let item = document.createElement('option')
-        item.text = `O número ${n} foi adicionado `
+        item.text = `Valor ${n} adicionado `
         item.value = `lis${n}`
         lis.appendChild(item)
+        
 
     }
     
 }
 function final(){
+    //lis.sorte()
+    n = Number(lis.length.value)
     let res = document.getElementById('res')
-        res.innerHTML= item.length
+    let kel = document.getElementById('kel')
+    res.innerHTML = `Ao todo temos ${lis.length} números cadastrados.`
+    kel.innerHTML+= lis[n-1]
+
+
 }
    
