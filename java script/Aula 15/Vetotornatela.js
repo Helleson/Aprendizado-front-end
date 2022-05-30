09f309d1cf6f67106bef8380d4dadaff43dda2f4
Helleson Sillva <souza.hellison@gmail.com>
2022-05-30 18:56:25 -0300

    h

--- java script/Aula 15/Vetotornatela.js
+++ java script/Aula 15/Vetotornatela.js
@@ -1,8 +1,8 @@
-let valores = [8,1,7,4,2,9]
+let valores = [8,1,7,4,2,]
 valores.sort()
 for(let pos in valores)
     console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
-    let pos = valores.indexOf(1)
+    let pos = valores.indexOf(8)
     if(pos == -1){
         console.log(`O valor não foi encontrado!`)
     }else{
--- java script/Aula 16/Desafio 03/Modelo/script.js
+++ java script/Aula 16/Desafio 03/Modelo/script.js
@@ -1,4 +1,140 @@
-    let lis = document.getElementById('lista')
+   let num = document.querySelector('input#num')
+   let lista = document.querySelector('select#lista')
+   let res = document.querySelector('div#res')
+   let valores = []
+   
+
+   function isNumero(n){
+       if(Number(n)>= 1 && Number(n)<=100){
+           return true
+       }else{
+           return false
+       }
+
+   }
+
+   function inLista(n, l){
+       if(l.indexOf(Number(n)) != -1){
+           return true
+       }else{
+           return false
+       }
+
+   }
+
+   function adicionar(){
+       if(isNumero(num.value) && !inLista(num.value, valores)){
+       valores.push(Number(num.value))
+       let n = Number(num.value)
+       let item = document.createElement('option')
+       item.text = `Valor ${n} adicionado `
+       item.value = `lis${n}`
+       lista.appendChild(item)
+      
+
+       }else{
+           window.alert('Valor inválido ou ja encontrado na lista.')
+       }
+   }
+   function final(){
+       let num = document.getElementById('num')
+       let n = Number(num.value)
+       let res = document.getElementById('res')  
+       let kel = document.getElementById('kel')  
+       res.innerHTML = `Ao todo temos ${valores.length} números cadastrados.`
+       
+ 
+        kel.innerHTML = `O maior valor informado foi ${max.valores}`
+`
+       
+        
+        
+        
+        
+
+   }
+
+    
+
+
+
+
+
+
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   /*let lis = document.getElementById('lista')
     
     function analise(){
    let num = document.getElementById('num')
@@ -7,13 +143,14 @@
     if(n<1 || n>100 ){
         alert('Valor invalido ou já encontrado na lista!')
     }else{
-        num.innerText= ' '
+      
         let item = document.createElement('option')
         item.text = `Valor ${n} adicionado `
-        item.value = `lis${n}`
+         
+         item.value = `lis${n}`
+       
         lis.appendChild(item)
-        
-        
+         
 
     }
     
@@ -26,8 +163,8 @@ function final(){
     let kel = document.getElementById('kel')
     res.innerHTML = `Ao todo temos ${lis.length} números cadastrados.`
     
-    kel.innerHTML += n
+    kel.innerHTML += analise(n)
 
 
-}
+}*/
    
